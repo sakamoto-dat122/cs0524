d9383d25bacd300059c7c31661a586b303619927
haruto nakamura <104044075+noah-zty@users.noreply.github.com>
2024-05-24 15:02:52 +0900

    コントロール

--- cs0524/Form1.Designer.cs
+++ cs0524/Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(318, 71);
+            button1.Name = "button1";
+            button1.Size = new Size(190, 79);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(106, 181);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 32);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Yu Gothic UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(367, 239);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(94, 35);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "坂本博斗";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- cs0524/Form1.cs
+++ cs0524/Form1.cs
@@ -6,5 +6,18 @@ namespace cs0524
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+            label1.Text = "Hello";
+            MessageBox.Show($"‚±‚ñ‚É‚¿‚Í{textBox1.Text}‚³‚ñ");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += 10;
+            label1.Top += 10;
+        }
     }
 }
